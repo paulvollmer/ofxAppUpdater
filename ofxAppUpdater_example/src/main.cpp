bbde3903c1dc57957b1d54b552c93d6b6da38306
Paul Vollmer <wrongentertainment@yahoo.de>
2012-04-05 13:09:36 +0200

    Added License to example files

--- ofxAppUpdater_example/src/main.cpp
+++ ofxAppUpdater_example/src/main.cpp
@@ -1,3 +1,32 @@
+/**
+ * ofxAppUpdater_example is developed by Paul Vollmer
+ * http://www.wng.cc
+ * 
+ * 
+ * Copyright (c) 2012 Paul Vollmer
+ *
+ * ofxAppUpdater_example is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * ofxAppUpdater_example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with ofxAppUpdater_example; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ * @author      Paul Vollmer
+ * @modified    2012.04.05
+ * @version     0.1.0
+ */
+
+
+
 #include "ofMain.h"
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
--- ofxAppUpdater_example/src/testApp.h
+++ ofxAppUpdater_example/src/testApp.h
@@ -1,8 +1,41 @@
+/**
+ * ofxAppUpdater_example is developed by Paul Vollmer
+ * http://www.wng.cc
+ * 
+ * 
+ * Copyright (c) 2012 Paul Vollmer
+ *
+ * ofxAppUpdater_example is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * ofxAppUpdater_example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with ofxAppUpdater_example; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ * @author      Paul Vollmer
+ * @modified    2012.04.05
+ * @version     0.1.0
+ */
+
+
+
 #pragma once
 
 #include "ofMain.h"
 #include "ofxAppUpdater.h"
+
 using namespace wng;
+
+
+
 
 
 class testApp : public ofBaseApp{
@@ -22,6 +55,7 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+		/* Added ofxAppUpdater addon */
 		ofxAppUpdater updater;
 
 };
