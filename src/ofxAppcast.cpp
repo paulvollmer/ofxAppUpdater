6f077a68a075724a67c525d4935bfbb6e97602be
Paul Vollmer <wrongentertainment@yahoo.de>
2012-04-23 10:58:35 +0200

    Added ofxAppcast class
    This class we use to get the values of appcast rss file. We have no
    variables to save at ofxAppcast class

---
+++ src/ofxAppcast.cpp
@@ -0,0 +1,172 @@
+/**
+ * ofxAppcast.cpp is developed by Paul Vollmer
+ * http://www.wng.cc
+ * 
+ * 
+ * Copyright (c) 2012 Paul Vollmer
+ *
+ * ofxAppcast.cpp is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * ofxAppcast.cpp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with ofxAppcast.cpp; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ * @author      Paul Vollmer
+ * @modified    2012.04.23
+ * @version     1.0.1d
+ */
+
+
+
+#include "ofxAppcast.h"
+
+
+namespace wng {
+	
+	ofxAppcast::ofxAppcast(){}
+	
+	
+	/**
+	 *
+	 * TODO: did we need to check if a tag exist?
+	 *
+	 * if(xml.tagExists("rss:channel:title", 0)){
+	 * return ofToString(xml.getValue("rss:channel:title", "ox11120119", 0));
+	 * } else {
+	 * return "not available";
+	 * }
+	 *
+	 */
+	
+	
+	/**
+	 * Get a tag from our first channel.
+	 */
+	
+	string ofxAppcast::getChannelTitle(ofxXmlSettings xml){
+		return ofToString(xml.getValue("rss:channel:title", "not available", 0));
+	}
+	
+	string ofxAppcast::getChannelLink(ofxXmlSettings xml){
+		return ofToString(xml.getValue("rss:channel:link", "not available", 0));
+	}
+	
+	string ofxAppcast::getChannelDescription(ofxXmlSettings xml){
+		return ofToString(xml.getValue("rss:channel:description", "not available", 0));
+	}
+	
+	string ofxAppcast::getChannelLanguage(ofxXmlSettings xml){
+		return ofToString(xml.getValue("rss:channel:language", "not available", 0));
+	}
+		
+	string ofxAppcast::getChannelPubDate(ofxXmlSettings xml){
+		return ofToString(xml.getValue("rss:channel:pubDate", "not available", 0));
+	}
+	
+	
+	
+	/**
+	 * Get a tag from a selected item.
+	 */
+	
+	string ofxAppcast::getTitle(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:title", "not available", which));
+	}
+	
+	string ofxAppcast::getDescription(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:description", "not available", which));
+	}
+	
+	string ofxAppcast::getPubDate(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:pubDate", "not available", which));
+	}
+	
+	string ofxAppcast::getEnclosureUrl(ofxXmlSettings xml, int which){
+		return ofToString(xml.getAttribute("rss:channel:item:enclosure", "url", "not available", which));
+	}
+	
+	string ofxAppcast::getEnclosureType(ofxXmlSettings xml, int which){
+		return ofToString(xml.getAttribute("rss:channel:item:enclosure", "type", "not available", which));
+	}
+	
+	
+	
+	/**
+	 * Get a Appcast tag from a selected item.
+	 */
+	
+	string ofxAppcast::getAppcastVersion(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastVersion", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastAuthor(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastAuthor", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastAuthorUrl(ofxXmlSettings xml, int which){
+		return ofToString(xml.getAttribute("rss:channel:item:appcastAuthor", "url", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastAuthorEmail(ofxXmlSettings xml, int which){
+		return ofToString(xml.getAttribute("rss:channel:item:appcastAuthor", "email", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastShortDescription(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastShortDescription", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastLicense(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastLicense", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastLicenseUrl(ofxXmlSettings xml, int which){
+		return ofToString(xml.getAttribute("rss:channel:item:appcastLicense", "url", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastHash(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastHash", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastHashAlgo(ofxXmlSettings xml, int which){
+		return ofToString(xml.getAttribute("rss:channel:item:appcastHash", "algo", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastRating(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastRating", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastRatingVotes(ofxXmlSettings xml, int which){
+		return ofToString(xml.getAttribute("rss:channel:item:appcastRating", "votes", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastDownloadCount(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastDownloadCount", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastKeywords(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastKeywords", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastDocLink(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastDocLink", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastSourceLink(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastSourceLink", "not available", which));
+	}
+	
+	string ofxAppcast::getAppcastPreviewLink(ofxXmlSettings xml, int which){
+		return ofToString(xml.getValue("rss:channel:item:appcastPreviewLink", "not available", which));
+	}
+	
+	
+}
---
+++ src/ofxAppcast.h
@@ -0,0 +1,85 @@
+/**
+ * ofxAppcast.h is developed by Paul Vollmer
+ * http://www.wng.cc
+ * 
+ * 
+ * Copyright (c) 2012 Paul Vollmer
+ *
+ * ofxAppcast.h is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * ofxAppcast.h is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with ofxAppcast.h; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ * 
+ * @author      Paul Vollmer
+ * @modified    2012.04.23
+ * @version     1.0.1d
+ */
+
+
+
+#pragma once
+
+#include <ofMain.h>
+#include "ofxXmlSettings.h"
+
+
+
+
+
+namespace wng {
+	
+	
+	class ofxAppcast {
+		
+	public:
+		ofxAppcast();
+		
+		
+		void load(string url);
+		// TODO: void remove();
+		
+		string getChannelTitle(ofxXmlSettings xml);
+		string getChannelLink(ofxXmlSettings xml);
+		string getChannelDescription(ofxXmlSettings xml);
+		string getChannelLanguage(ofxXmlSettings xml);
+		string getChannelPubDate(ofxXmlSettings xml);
+		
+		string getTitle(ofxXmlSettings xml, int which);
+		string getDescription(ofxXmlSettings xml, int which);
+		string getPubDate(ofxXmlSettings xml, int which);
+		string getEnclosureUrl(ofxXmlSettings xml, int which);
+		string getEnclosureType(ofxXmlSettings xml, int which);
+		
+		string getAppcastVersion(ofxXmlSettings xml, int which);
+		string getAppcastAuthor(ofxXmlSettings xml, int which);
+		string getAppcastAuthorUrl(ofxXmlSettings xml, int which);
+		string getAppcastAuthorEmail(ofxXmlSettings xml, int which);
+		string getAppcastShortDescription(ofxXmlSettings xml, int which);
+		string getAppcastLicense(ofxXmlSettings xml, int which);
+		string getAppcastLicenseUrl(ofxXmlSettings xml, int which);
+		string getAppcastHash(ofxXmlSettings xml, int which);
+		string getAppcastHashAlgo(ofxXmlSettings xml, int which);
+		string getAppcastRating(ofxXmlSettings xml, int which);
+		string getAppcastRatingVotes(ofxXmlSettings xml, int which);
+		string getAppcastDownloadCount(ofxXmlSettings xml, int which);
+		string getAppcastKeywords(ofxXmlSettings xml, int which);
+		string getAppcastDocLink(ofxXmlSettings xml, int which);
+		string getAppcastSourceLink(ofxXmlSettings xml, int which);
+		string getAppcastPreviewLink(ofxXmlSettings xml, int which);
+		
+		
+		ofxXmlSettings xml;
+		
+	};
+	
+};
