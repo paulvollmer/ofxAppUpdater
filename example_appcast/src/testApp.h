3397cbae1d6fd7550837fdb5806def2a557c8af4
Paul Vollmer <wrongentertainment@yahoo.de>
2012-04-25 10:43:25 +0200

    delete unused oF events

--- example_appcast/src/testApp.h
+++ example_appcast/src/testApp.h
@@ -48,13 +48,6 @@ public:
 	
 	void keyPressed(int key);
 	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
 	void urlResponse(ofHttpResponse & response);  
 	void exit();
 		
