1cac9d76395b5b16b8a4abcd4c5568983a6e697b
Paul Vollmer <wrongentertainment@yahoo.de>
2012-04-25 10:13:27 +0200

    changes at UpdaterApp nsmenubar

--- UpdaterApp/src/main.cpp
+++ UpdaterApp/src/main.cpp
@@ -31,12 +31,54 @@
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
 
+// Using NSMenu outside the NIB
+// Found at openFrameworks forum. http://forum.openframeworks.cc/index.php/topic,78.0.html (Reply #6)
+// IMPORTANT:
+// right-click on that file (in my case main.cpp), select "Get Info",
+// then in the "General" tab set your "File Type" to sourcecode.cpp.objcpp
+#include "Cocoa/Cocoa.h"
+
 //========================================================================
 int main( ){
 
     ofAppGlutWindow window;
 	ofSetupOpenGL(&window, 800,400, OF_WINDOW);			// <-------- setup the GL context
 
+	
+	// Modified menubar
+	if (NSApp){  
+        NSMenu      *menu;  
+        NSMenuItem  *menuItem;   
+		
+        [NSApp setMainMenu:[[NSMenu alloc] init]];  
+		
+		
+		// Appname menu
+		menu = [[NSMenu alloc] initWithTitle:@""];  
+		[menu addItemWithTitle:@"About UpdateManager" action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""];
+		
+		[menu addItem:[NSMenuItem separatorItem]];
+		
+		[menu addItemWithTitle:@"Hide UpdateManager" action:@selector(hide:) keyEquivalent:@"h"];
+		
+		menuItem = (NSMenuItem *)[menu addItemWithTitle:@"Hide Others" action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
+		[menuItem setKeyEquivalentModifierMask:(NSAlternateKeyMask|NSCommandKeyMask)];
+		
+		[menu addItemWithTitle:@"Show All" action:@selector(unhideAllApplications:) keyEquivalent:@""];
+		
+		[menu addItem:[NSMenuItem separatorItem]];
+		
+		[menu addItemWithTitle:@"Quit UpdateManager" action:@selector(terminate:) keyEquivalent:@"q"]; 
+		
+		// Put menu into the menubar
+		menuItem = [[NSMenuItem alloc] initWithTitle:@"Apple" action:nil keyEquivalent:@""];  
+		[menuItem setSubmenu:menu];  
+		[[NSApp mainMenu] addItem:menuItem];
+		// Tell the application object that this is now the application menu
+		//[NSApp setMainMenu:menu];
+    } 
+	
+	
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
