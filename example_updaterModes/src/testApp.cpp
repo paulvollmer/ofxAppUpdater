124aed50a25b58b16a4f4c8b5671d744809e70a0
Paul Vollmer <wrongentertainment@yahoo.de>
2012-04-25 15:21:19 +0200

    delete unused oF events

--- example_updaterModes/src/testApp.cpp
+++ example_updaterModes/src/testApp.cpp
@@ -182,44 +182,6 @@ void testApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
-
-void testApp::exit(){
-	
-}
-
 void testApp::urlResponse(ofHttpResponse & response){
 	if(response.status == 200 && response.request.name == "async_req") {
 		cout << "loading" << endl;
